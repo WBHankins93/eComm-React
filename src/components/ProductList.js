12eca8e3f6c816eae137f1a94a2a244010edccfa
Ben <WBHankins93@Bens-Air.attlocal.net>
2019-07-16 23:24:21 -0500

    added and styled products title

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import Title from './Title.js'
 import Product from './Product.js'
 
 export default class ProductList extends Component {
+    state={
+        products:[]
+    }
     render() {
         return (
-            <div>
-                <Product />
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                   <div className="container">
+                        <Title name="our" title="products" />
+                        <div className="row">
+                            
+                        </div>
+                   </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
