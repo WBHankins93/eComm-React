7206a789f12b2e318424dc9504e875810285170d
Ben <WBHankins93@Bens-Air.attlocal.net>
2019-07-16 23:57:58 -0500

    used context to map through each listing. working to style now

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 import Title from './Title.js'
 import {storeProducts} from '../data'
+import {ProductConsumer} from '../context';
 import Product from './Product.js'
 
 export default class ProductList extends Component {
     state={
-        products:[]
+        products:storeProducts
     }
     render() {
+        
+
         return (
             <React.Fragment>
                 <div className="py-5">
                    <div className="container">
                         <Title name="our" title="products" />
                         <div className="row">
-                            
+                            <ProductConsumer>
+                                {value=>{
+                                    return value.products.map( product => {
+                                        return <Product key={product.id} product={product}/>;
+                                    })
+                                }}
+                            </ProductConsumer>
                         </div>
                    </div>
                 </div>
