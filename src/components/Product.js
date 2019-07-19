1ee9bd6366139ea901655572101e560866d3a949
Ben <WBHankins93@Bens-MacBook-Air.local>
2019-07-18 22:10:55 -0500

    added footer to product page that displays product title and price

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,7 +5,7 @@ import { ProductConsumer } from '../context';
 
 export default class Product extends Component {
     render() {
-        const {id, title, img, inCart} = this.props.product;
+        const {id, title, img, inCart, price} = this.props.product;
         return (
             <ProductWrapper className="col-9 mx-auto col-md col-lg-3 my-3">
                 <div className="card">
@@ -15,9 +15,19 @@ export default class Product extends Component {
                         </Link> 
                         <button className="cart-btn" disabled={inCart ? true:false} onClick={()=>{console.log('added to cart')}}>
                             {inCart?(
-                            <p className="text-capitalize mb-0" disabled></p>):(<i className="fas fa-cart-plus" />)}
+                            <p className="text-capitalize mb-0" disabled>{""} In cart</p>):(<i className="fas fa-cart-plus" />)}
                         </button>
                         
+                    </div>
+                    {/* card footer*/}
+                    <div className="card-footer d-flex jusitfy-content-between">
+                        <p className="align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span>$</span>
+                            {price}
+                        </h5>
                     </div>
                 </div>
             </ProductWrapper>
